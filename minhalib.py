e33943700c4f598c278d65e0d9ac4d612a1be216
alMayrink <50755596+alMayrink@users.noreply.github.com>
2022-11-26 18:20:38 -0300

    Create minhalib.py

---
+++ minhalib.py
@@ -0,0 +1,8 @@
+class Carregador:
+ 
+    def carregar_dados(self, url: str, atributos: list):
+        """ Carrega e retorna um DataFrame. Há diversos parâmetros 
+        no read_csv que poderiam ser utilizados para dar opções 
+        adicionais.
+        """
+        return pd.read_csv(url, names=atributos)
